04191c6493a38ae43bdbbb27683f595b9b4bdf2c
Ivan <ivan12kalininn@gmail.com>
2023-11-16 16:13:30 +0300

    First version. Not working. Ui and file structure is done.

---
+++ main.py
@@ -0,0 +1,15 @@
+import sqlite3
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    ex.show()
+    sys.exit(app.exec_())
